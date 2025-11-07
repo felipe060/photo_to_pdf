978925200d4e95a702ccdd79b62553bfcf26e92a
felipe060 <felipeflip29@hotmail.com>
2025-11-07 11:14:51 -0300

    first commit

---
+++ index.py
@@ -0,0 +1,88 @@
+from flask import Flask, render_template, url_for, request, send_from_directory
+import os
+
+app = Flask(__name__)
+app.config.from_object("config.ProductionConfig")
+
+os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+os.makedirs(app.config["OUTPUT_FOLDER"], exist_ok=True)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.errorhandler(413)          #verifica o tamanho do arquivo
+def request_too_large(error):   #se ele for maior doq o MAX_CONTENT_LENGTH, o resto do codigo nem vai ser executado
+    return ("file size exceeds maximum of 50Mb "
+            "<br>"
+            "arquivo maior doq o limite de 50Mb permitido")
+
+
+@app.route("/upload_image", methods=["GET", "POST"])
+def upload_image():
+
+    print("index.py request.files -->", request.files)                   #imprime o dicionario do request
+    print("")
+
+    if "name_image" not in request.files:                       #se o arquivo nomeado "name_image" no html n tiver no request.files
+        print("no files were sent in the request\n"             #imprime no terminal esse texto dizendo q o arquivo n ta la
+              "nenhum arquivo foi enviado na requisicao\n")
+        return ("none file was sent in request <br> "           #e retorna pro usuario dizendo q n tem nenhuma arquivo na requisicao
+                "nenhum arquivo foi enviado na requisicao")
+
+    file = request.files["name_image"]                          #armazena numa variavel o arquivo que o usuario fez o upload
+    print("index.py file-->", file)                             #imprime no terminal essa variavel
+    print("")
+
+    if file.filename == "":                                                 #se o arquivo que armazenamos na variavel ali em cima, for uma string vazia
+        print("empty filename // filename is a empty string\n"              #dai imprime no terminal q o nome do arquivo ta como string vazia
+              "filename vazio // filename esta como uma string vazia")
+        return "empty filename <br> filename vazio"                         #e retorna pro usuario dizendo que o nome do arquivo ta vazio
+
+    if file:                                                                #se a variavel "file" retorna True, ou seja, ela existe e n ta como string vazio
+        file.seek(0, 2)
+        tamanho = file.tell()
+        file.seek(0)
+        print("index.py tamanho -->", tamanho, "bytes")
+        print("")
+
+        from side_tasks import verify_extension
+        verify_extension(file_name=file)
+
+        r'''upload_folder = app.config["UPLOAD_FOLDER"]
+        nome_arquivo = file.filename
+        print("nome_arquivo -->", nome_arquivo)
+        print("nome_arquivo type -->", type(nome_arquivo))
+        print("")
+
+        extensao_arquivo = os.path.splitext(nome_arquivo)[1]
+        print("extensao_arquivo --> ", extensao_arquivo)
+
+        new_file_base_name = str(uuid.uuid4())
+        print("new_file_base_name -->", new_file_base_name)
+        print("")
+
+        new_file_name = str(new_file_base_name + extensao_arquivo)
+        print("new_file_name --> ", new_file_name)
+        print("new_file_name type --> ", type(new_file_name))
+        print("")
+
+        file_path = os.path.join(upload_folder, new_file_name)
+
+        print("file_path index.py dentro da funcao depois de mudar -->", file_path)
+        print("file_path index.py type dentro da funcao depois de mudar -->", type(file_path))
+        print("")
+        file.save(file_path)
+
+        from image_converter import convert_to_pdf
+        print(convert_to_pdf(image_name=new_file_name))
+
+        if file_extension:
+            pass'''
+        return send_from_directory("final_doc", "output.pdf", as_attachment=True)
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ side_tasks.py
@@ -0,0 +1,71 @@
+import os
+
+
+def verify_extension(**kwargs):             #function that verifies if the extension of the file the user uploaded, is within the allowed extensions
+    from index import app
+    import os
+    import uuid
+
+    upload_folder = app.config["UPLOAD_FOLDER"]
+    nome_arquivo = kwargs["file_name"].filename
+    arquivo = kwargs["file_name"]
+
+    print("side_tasks.py nome_arquivo --> ", nome_arquivo)
+    print("side_tasks.py nome_arquivo type --> ", type(nome_arquivo))
+    print("")
+
+    print("side_tasks.py arquivo -->", arquivo)
+    print("side_tasks.py arquivo type -->", type(arquivo))
+    print("")
+
+    extensao_arquivo = os.path.splitext(nome_arquivo)[1]
+    print("side_tasks.py extensao_arquivo -->", extensao_arquivo)
+    print("")
+
+    if extensao_arquivo in app.config["ALLOWED_EXTENSIONS"]:
+        new_file_base_name = str(uuid.uuid4())
+        print("side_tasks.py new_file_base_name -->", new_file_base_name)
+        print("")
+
+        new_file_name = str(new_file_base_name + extensao_arquivo)
+        print("side_tasks.py new_file_name -->", new_file_name)
+        print("side_tasks.py new_file_name type --> ", type(new_file_name))
+        print("")
+
+        file_path = os.path.join(upload_folder, new_file_name)
+        print("side_tasks.py file_path -->", file_path)
+        print("")
+
+        arquivo.save(file_path)
+
+        print(f"side_tasks.py file extension is allowed --> {extensao_arquivo}")
+        print("")
+
+        convert_to_pdf(image_name=new_file_name)
+        return f"file extension is allowed --> {extensao_arquivo}"
+
+    else:
+        print(f"side_tasks.py file extension // not // allowed --> {extensao_arquivo}")
+        return f"side_tasks.py file extension not allowed --> {extensao_arquivo}"
+
+
+def convert_to_pdf(**kwargs):
+    from img2pdf import convert
+
+    image_name = kwargs["image_name"]
+    print("side_tasks.py convert_to_pdf() image_name kwargs['image_name'] --> ", image_name)
+    image = f"uploads/{image_name}"
+
+    print("side_tasks.py convert_to_pdf() kwargs --> ", kwargs)
+    print("")
+
+    #print("image_converter.py asking /file_path/ to index.py")
+    #print("file path image_converter.py -->", file_path)
+
+    foto_convertida = convert(image)
+    with open("final_doc/output.pdf", "wb") as file:
+        file.write(foto_convertida)
+
+    ola = os.remove(f"uploads/{image_name}")
+    print("side_tasks.py convert_to_pdf() ola -->", ola)
+    return "foto convertida"
